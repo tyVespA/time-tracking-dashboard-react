a13fb96d163a9370e418a9b0e58a660499f23f8d
tyVespA <migliavacca.marco08@gmail.com>
2023-02-08 14:15:26 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import TestComponent from "./components/TestComponent";
+
+function App() {
+  return (
+    <div>
+      <TestComponent />
+    </div>
+  );
+}
+
+export default App;
